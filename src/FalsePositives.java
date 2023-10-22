7e001f227749ad8eb55f13b83b3f2b7aeb46185d
shiv-neel <shiv.neel1622@gmail.com>
2023-10-22 14:52:57 -0500

    wip

--- src/FalsePositives.java
+++ src/FalsePositives.java
@@ -1,59 +1,73 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 
 
 public class FalsePositives {
-    public static void main(String[] args) throws IOException {
-        int filterSize = 40000;
-        int setSize = 10000;
-        int bitsPerElement = filterSize / setSize;
-        int expectedInsertions = 1000; 
-        double falsePositiveRate = 0.01; 
+    private static final int setSize = 10000; // size of data set (N)
 
-        // Create the three random bloom filters
-        BloomFilterRan bfr = new BloomFilterRan(setSize, bitsPerElement);
-        BloomFilterRanPlus bfrp = new BloomFilterRanPlus(setSize, bitsPerElement);
-        MultiMultiBloomFilter mmbf = new MultiMultiBloomFilter(setSize, expectedInsertions);
+    private static final bitsPerElement = 8; // number of bits per element (b)
+    private static final int filterSize = ; // size of bloom filter (M)
+
+
+    public static void main(String[] args) throws IOException {
+        final String database = "src/data/fp_database.txt";
+        final String differential = "src/data/fp_test.txt";
+
+        BloomFilterFNV bloomFilterFNV = new BloomFilterFNV(setSize, bitsPerElement);
+        BloomFilterRan bloomFilterRan = new BloomFilterRan(setSize, bitsPerElement);
+        BloomFilterRanPlus bloomFilterRanPlus = new BloomFilterRanPlus(setSize, bitsPerElement);
+        MultiMultiBloomFilter multiMultiBloomFilter = new MultiMultiBloomFilter(setSize, bitsPerElement);
 
         // add words to bloom filters
-        loadWordsIntoBFR("FreqWords.txt", bfr);
-        loadWordsIntoBFRP("FreqWords.txt", bfrp);
-        loadWordsIntoMMBF("FreqWords.txt", mmbf);
+
+        loadWordsIntoBloomFilterRan(database, bloomFilterRan);
+        loadWordsIntoBloomFilterRanPlus(database, bloomFilterRanPlus);
+        loadWordsIntoMultiMultiBloomFilter(database, multiMultiBloomFilter);
 
         // test bloom filters for positives
-        testFalsePositives("test_file.txt", bloomFilter);
+        testFalsePositivesForBloomFilterRan(differential, bloomFilterRan);
     }
 
-    private static void loadWordsIntoBFR(String filename, BloomFilterRan bfr) throws IOException {
+    private static void loadWordsIntoBloomFilterFNV(String filename, BloomFilterFNV bloomFilterFNV) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                bfr.add(line);
+                bloomFilterFNV.add(line);
             }
         }
     }
 
-    private static void loadWordsIntoBFRP(String filename, BloomFilterRanPlus bfpr) throws IOException {
+    private static void loadWordsIntoBloomFilterRan(String filename, BloomFilterRan bloomFilterRan) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                bfpr.add(line);
+                bloomFilterRan.add(line);
             }
         }
     }
 
-    private static void loadWordsIntoMMBF(String filename, MultiMultiBloomFilter mmbf) throws IOException {
+    private static void loadWordsIntoBloomFilterRanPlus(String filename, BloomFilterRanPlus bloomFilterRanPlus) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
             String line;
             while ((line = reader.readLine()) != null) {
-                mmbf.add(line);
+                bloomFilterRanPlus.add(line);
             }
         }
     }
 
-    private static void testFalsePositives(String testFilename, BloomFilter<String> bloomFilter) throws IOException {
+    private static void loadWordsIntoMultiMultiBloomFilter(String filename, MultiMultiBloomFilter multiMultiBloomFilter) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                multiMultiBloomFilter.add(line);
+            }
+        }
+    }
+
+    private static void testFalsePositivesForBloomFilterFNV(String testFilename, BloomFilterFNV bloomFilterFNV) throws IOException {
         int falsePositives = 0;
         int totalItems = 0;
 
@@ -61,7 +75,26 @@ public class FalsePositives {
             String line;
             while ((line = reader.readLine()) != null) {
                 totalItems++;
-                if (bloomFilter.mightContain(line)) {
+                if (bloomFilterFNV.appears(line)) {
+                    falsePositives++;
+                }
+            }
+        }
+
+        System.out.println("Total items tested: " + totalItems);
+        System.out.println("False positives: " + falsePositives);
+        System.out.println("False positive rate: " + (falsePositives / (double) totalItems));
+    }
+
+    private static void testFalsePositivesForBloomFilterRan(String testFilename, BloomFilterRan bloomFilterRan) throws IOException {
+        int falsePositives = 0;
+        int totalItems = 0;
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(testFilename))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                totalItems++;
+                if (bloomFilterRan.appears(line)) {
                     falsePositives++;
                 }
             }
